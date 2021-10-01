7954141f1f9e8168d23cb3d739fc57a22f8d13ac
Mustafa Ozan Çetin <gelozan6@gmail.com>
2021-10-01 18:26:13 +0300

    Revert "Revert "Proje dosyası ekle.""
    This reverts commit cc48bec235f9281f92bfc796992f196e564bc6d0.

---
+++ DetailsWindow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HistoryFetcher
+{
+    public partial class DetailsWindow : Form
+    {
+        private string title;
+        private string url;
+
+        public DetailsWindow(string title, string url)
+        {
+            InitializeComponent();
+            this.title = title;
+            this.url = url;
+            label2.Text = this.title;
+            linkLabel1.Text = this.url;
+            linkLabel1.Links.Add(0, this.url.Length, this.url);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.url);
+            MessageBox.Show("Url copied!");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(this.url);
+        }
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HistoryFetcher
+{
+    public class Item
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public DateTime date { get; set; }
+    }
+}
